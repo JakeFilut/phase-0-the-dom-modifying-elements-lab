ea167474cc52936af419195188609c08121da6f8
Jake <jakefilutsoftware@gmail.com>
2021-10-01 16:24:02 -0600

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.idname = "victory";
+newHeader.setAttribute("id", "victory");
+
+const string = document.createElement('p');
+string.innerHTML = "Jake is the champion";
+//newHeader.h1(p);
+newHeader.appendChild(string);
